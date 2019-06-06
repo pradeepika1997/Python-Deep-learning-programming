3eb9248b08af6048f758e279586f56bc247eb168
Pradeepika Kolluru <kollurupradeepika@gmail.com>
2019-06-06 14:46:26 -0500

    Adding ICP-2

--- ICP-2/Source code/wordcount.py
+++ ICP-2/Source code/wordcount.py
@@ -2,9 +2,10 @@ fileName = input("Enter file name : ")
 f = open(fileName,'r')
 wordcount={}
 for str in f.read().split():
-    if str not in wordcount:
-        wordcount[str] = 1
+    temp = str.lower()
+    if temp not in wordcount:
+        wordcount[temp] = 1
     else:
-        wordcount[str] += 1
+        wordcount[temp] += 1
 print("Output written in file")
 print(wordcount, file=open("output.txt", "w"))
